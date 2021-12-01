b8c9e12fdc680698a267bfad6a7e610460a702e6
Serp <manu.serpette74@gmail.com>
2021-12-01 16:18:56 +0100

    input tool is working

--- input_tool/get_input.sh
+++ input_tool/get_input.sh
@@ -1,2 +1,11 @@
+#!/bin/bash
+
 sqlite3 /home/manu/.mozilla/firefox/jfubr9dg.default-release/cookies.sqlite < sql_command 
 
+node parse.mjs
+
+rm cookie
+
+wget --load-cookies cookie.txt https://adventofcode.com/2021/day/$1/input
+
+rm cookie.txt
--- input_tool/parse.mjs
+++ input_tool/parse.mjs
@@ -1,7 +1,42 @@
-import { readFileSync } from "fs";
+import { readFileSync, writeFileSync } from "fs";
 var array = readFileSync("cookie").toString().split("\n");
+array=array.slice(1,array.length-1);
 
-array=array.split("|");
+array = array.map((val) => {
+  return val.split("|");
+});
 
+const toCookie = (ck) => {
+  let flag;
 
-console.log(array)
+  if (parseInt(ck[9], 10) === 1) {
+    flag = "TRUE";
+  } else {
+    flag = "FALSE";
+  }
+
+  let line =
+    ck[4] +
+    "\t" +
+    "TRUE" +
+    "\t/\t" +
+    flag +
+    "\t" +
+    ck[6] +
+    "\t" +
+    ck[2] +
+    "\t" +
+    ck[3] +
+    "\n";
+  return line;
+};
+
+let data = "# Netscape HTTP Cookie File\n\n";
+
+array.forEach((val) => {
+  data = data + toCookie(val);
+});
+
+console.log(data);
+
+writeFileSync('cookie.txt',data)
