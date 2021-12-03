3b4a609b2dcc83bdf1b3a89d2e36aec092915c77
DatBen <benjamindumont@live.com>
2021-12-03 11:58:16 +0100

    p2 not ok

--- day3/JavaScript/src/p2.mjs
+++ day3/JavaScript/src/p2.mjs
@@ -1,3 +1,56 @@
 import { readFileSync } from "fs";
 var array = readFileSync("data/data.txt").toString().split("\n");
 const toInt = (arr) => arr.map((i) => parseInt(i, 10));
+
+let ox = array;
+let co = array;
+
+const reduce_ox = (arr, i) => {
+  let res = arr.reduce(
+    (res, value) => {
+      if (value[i] === "0") {
+        return [res[0] + 1, res[1]];
+      }
+      return [res[0], res[1] + 1];
+    },
+    [0, 0]
+  );
+
+  if (res[1] >= arr.length / 2) {
+    return "1";
+  }
+
+  return "0";
+};
+
+const reduce_co = (arr, i) => {
+  let res = arr.reduce(
+    (res, value) => {
+      if (value[i] === "0") {
+        return [res[0] + 1, res[1]];
+      }
+      return [res[0], res[1] + 1];
+    },
+    [0, 0]
+  );
+  if (res[0] >= arr.length / 2) {
+    return "0";
+  }
+  return "1";
+};
+
+let index = 0;
+while (ox.length > 1) {
+  let res = reduce_ox(ox, index);
+
+  ox = ox.filter((value) => value[index] === res);
+  index++;
+}
+index = 0;
+while (co.length > 1) {
+  let res = reduce_co(co, index);
+  co = co.filter((value) => value[index] === res);
+  index++;
+}
+
+console.log(parseInt(ox, 2) * parseInt(co, 2));
