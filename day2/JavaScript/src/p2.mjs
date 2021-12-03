1864d996237fa1106b2d9cfb3dab4600210f6d53
Serp <manu.serpette74@gmail.com>
2021-12-03 10:39:23 +0100

    d2

---
+++ day2/JavaScript/src/p2.mjs
@@ -0,0 +1,33 @@
+import { readFileSync } from "fs";
+var array = readFileSync("data/input").toString().split("\n");
+const toInt = (arr) => arr.map((i) => parseInt(i, 10));
+
+let x=0;
+let depth=0;
+let aim=0;
+for (let i =0;i<array.length;i++){
+
+    let vl = array[i].split(" ")
+    switch (vl[0]) {
+        case "forward":
+            x=x+parseInt(vl[1],10);
+            depth=depth+aim*parseInt(vl[1],10);
+            break;
+        case "up":
+            aim=aim-parseInt(vl[1],10) 
+            break;
+        case "down":
+            aim=aim+parseInt(vl[1],10)    
+    }
+
+
+}
+console.log(x)
+console.log(depth)
+console.log(aim)
+console.log(x*depth)
+        
+
+
+
+7139340
