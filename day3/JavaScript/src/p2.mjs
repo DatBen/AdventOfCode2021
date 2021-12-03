5c0d3b9d5bf7c7440f88329cbf0136e51acb51ff
Serp <manu.serpette74@gmail.com>
2021-12-03 12:26:55 +0100

    c bon

--- day3/JavaScript/src/p2.mjs
+++ day3/JavaScript/src/p2.mjs
@@ -2,20 +2,22 @@ import { readFileSync } from "fs";
 var array = readFileSync("data/input").toString().split("\n");
 const toInt = (arr) => arr.map((i) => parseInt(i, 10));
 
-let bst = array.reduce(
-  (acc, val) => {
-    for (let i = 0; i < val.length; i++) {
-      if (parseInt(val.charAt(i), 10) === 1) {
-        acc[i][1] = acc[i][1] + 1;
-      } else {
-        acc[i][0] = acc[i][0] + 1;
+const bst = (array) => {
+  return array.reduce(
+    (acc, val) => {
+      for (let i = 0; i < val.length; i++) {
+        if (parseInt(val.charAt(i), 10) === 1) {
+          acc[i][1] = acc[i][1] + 1;
+        } else {
+          acc[i][0] = acc[i][0] + 1;
+        }
       }
-    }
 
-    return acc;
-  },
-  Array.from({ length: array[0].length }, () => [0, 0])
-);
+      return acc;
+    },
+    Array.from({ length: array[0].length }, () => [0, 0])
+  );
+};
 
 const max = (a) => {
   if (a[0] > a[1]) {
@@ -25,12 +27,20 @@ const max = (a) => {
   }
 };
 
-const oxy_find = (bt, arr) => {
+const min = (a) => {
+  if (a[0] <= a[1]) {
+    return "0";
+  } else {
+    return "1";
+  }
+};
+
+const oxy_find = (arr) => {
   let flag = true;
   let stock = [];
   let i = 0;
   while (flag) {
-    let v = max(bt[i]);
+    let v = max(bst(arr)[i]);
     arr.forEach((el) => {
       if (el.charAt(i) === v) {
         stock.push(el);
@@ -38,13 +48,47 @@ const oxy_find = (bt, arr) => {
     });
     if (stock.length === 1) {
       flag = false;
+      arr = stock;
+
     } else {
       i++;
+      arr = stock;
       stock = [];
     }
   }
-  return stock;
+  return arr;
 };
 
+const c02_find = (arr) => {
+  let flag = true;
+  let stock = [];
+  let i = 0;
+  while (flag) {
+    let v = min(bst(arr)[i]);
+    arr.forEach((el) => {
+      if (el.charAt(i) === v) {
+        stock.push(el);
+      }
+    });
+    if (stock.length === 1) {
+      flag = false;
+      arr = stock;
 
-console.log(oxy_find(bst,array))
+    } else {
+      i++;
+      arr = stock;
+      stock = [];
+    }
+    console.log("tgjhgk");
+    console.log(arr);
+  }
+  return arr;
+};
+
+let o = oxy_find(array);
+let c = c02_find(array);
+
+console.log(o);
+console.log(c);
+
+console.log(parseInt(o, 2) * parseInt(c, 2));
