02ff8417ea991a40f30f292e037165d38221dd79
DatBen <benjamindumont@live.com>
2021-12-04 10:34:50 +0100

    p1 done

--- day4/JavaScript/src/p1.mjs
+++ day4/JavaScript/src/p1.mjs
@@ -2,8 +2,6 @@ import { readFileSync } from "fs";
 var array = readFileSync("data/data.txt").toString().split("\n");
 const toInt = (arr) => arr.map((i) => parseInt(i, 10));
 
-const draw = toInt(array[0].split(","));
-
 const init_boards = () => {
   let boards = [];
   for (let i = 0; i < 100; i++) {
@@ -17,5 +15,51 @@ const init_boards = () => {
   return boards;
 };
 
+const transpose = (board) => {
+  return board[0].map((_, colIndex) => board.map((row) => row[colIndex]));
+};
+
+const has_won = (board, drawn) => {
+  if (drawn.lenght < 5) {
+    return false;
+  }
+  const transposed = transpose(board);
+  for (let i = 0; i < 5; i++) {
+    if (board[i].every((element) => drawn.includes(element))) {
+      return true;
+    }
+    if (transposed[i].every((element) => drawn.includes(element))) {
+      return true;
+    }
+  }
+  return false;
+};
+
+const solution = (board, drawn) => {
+  let sum = board.flat().reduce((res, element) => {
+    if (drawn.includes(element)) {
+      return res;
+    } else {
+      return res + element;
+    }
+  }, 0);
+  return sum * drawn[drawn.length - 1];
+};
+
+const draw = toInt(array[0].split(","));
 const boards = init_boards();
-console.log(boards);
+let i = 5;
+let unfounded = true;
+let winning_board;
+let drawn;
+
+while (i <= draw.length && unfounded) {
+  drawn = draw.slice(0, i);
+  winning_board = boards.find((element) => has_won(element, drawn));
+  if (winning_board != undefined) {
+    unfounded = false;
+  }
+  i++;
+}
+
+console.log(solution(winning_board, drawn));
