4db5ae1b6327ccb94dd01b4b1cc47e1c5e15c44a
Serp <manu.serpette74@gmail.com>
2021-12-06 15:44:30 +0100

    day 6

--- day6/JavaScript/src/p2.mjs
+++ day6/JavaScript/src/p2.mjs
@@ -1,3 +1,37 @@
-import { readFileSync } from 'fs';
-var array = readFileSync('data/input').toString().split('\n');
+import { readFileSync } from "fs";
+var array = readFileSync("data/input").toString().split("\n");
 const toInt = (arr) => arr.map((i) => parseInt(i, 10));
+
+array = toInt(array[0].split(","));
+
+let sim = Array.from({ length: 9 }, () => 0);
+
+const cvt = (arr, sim) => {
+  arr.forEach((element) => {
+    sim[element]++;
+  });
+  return sim;
+};
+
+const oneDay = (sim) => {
+  let res = Array.from({ length: 9 }, () => 0);
+  for (let i = 0; i < 8; i++) {
+    res[i] = sim[i + 1];
+  }
+  res[6] = sim[0] + res[6];
+  res[8] = res[8] + sim[0];
+  return res;
+};
+
+const simu = (n, sim) => {
+    
+  for (let i = 0; i < n; i++) {
+
+    sim = oneDay(sim);
+  }
+  console.log(sim)
+  return sim.reduce((acc,vl)=>acc+vl);
+};
+
+
+console.log(simu(256,cvt(array,sim)))
