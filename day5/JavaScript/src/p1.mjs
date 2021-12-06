fccc481adbcc7634248c17049f516a8164d5e8d3
DatBen <benjamindumont@live.com>
2021-12-06 18:17:13 +0100

    update package

--- day5/JavaScript/src/p1.mjs
+++ day5/JavaScript/src/p1.mjs
@@ -1,3 +1,7 @@
-import { readFileSync } from 'fs';
-var array = readFileSync('data/input').toString().split('\n');
+import { readFileSync } from "fs";
+var array = readFileSync("data/input").toString().split("\n");
 const toInt = (arr) => arr.map((i) => parseInt(i, 10));
+
+const data = array.split("->");
+
+console.log("->");
