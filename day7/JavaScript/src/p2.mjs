ed9ef6d2c43df2bde0a04b45ae15fd8776140bcb
DatBen <benjamindumont@live.com>
2021-12-07 13:48:25 +0100

    p2 is ok

--- day7/JavaScript/src/p2.mjs
+++ day7/JavaScript/src/p2.mjs
@@ -2,20 +2,22 @@ import { readFileSync } from "fs";
 var array = readFileSync("data/input").toString().split("\n");
 const toInt = (arr) => arr.map((i) => parseInt(i, 10));
 
-const data = toInt(array[0].split(","));
-
-const mean = (a) => {
-  return Math.round(a.reduce((res, value) => value + res, 0) / a.length);
-};
+const file = toInt(array[0].split(","));
 
 const sum = (int) => {
   return (int * (int + 1)) / 2;
 };
 
-const fuel_cost = (data) => {
-  const moy = mean(data);
-
-  return data.reduce((res, value) => sum(Math.abs(moy - value)) + res, 0);
+const solution = (data) => {
+  const max = Math.max(...data);
+  let res = data.reduce((res, value) => sum(value) + res, 0);
+  for (let i = 1; i <= max; i++) {
+    let temp = data.reduce((res, value) => sum(Math.abs(i - value)) + res, 0);
+    if (temp < res) {
+      res = temp;
+    }
+  }
+  return res;
 };
 
-console.log(fuel_cost([16, 1, 2, 0, 4, 2, 7, 1, 2, 14]));
+console.log(solution(file));
