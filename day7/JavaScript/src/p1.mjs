0ef5e0fd683fbc62879715c5fa01dba24c47504d
Serp <manu.serpette74@gmail.com>
2021-12-07 23:36:15 +0100

    day 7 p1

---
+++ day7/JavaScript/src/p1.mjs
@@ -0,0 +1,44 @@
+import { readFileSync } from "fs";
+var array = readFileSync("data/input").toString().split("\n");
+const toInt = (arr) => arr.map((i) => parseInt(i, 10));
+
+array = toInt(array[0].split(","));
+
+const max = (arr) => {
+  return arr.reduce((acc, vl) => {
+    if (vl > acc) {
+      return vl;
+    } else {
+      return acc;
+    }
+  });
+};
+
+const abs = (num) => {
+  if (num >= 0) {
+    return num;
+  } else {
+    return -num;
+  }
+};
+const test_pos = (n, arr) => {
+  return arr.reduce((acc, vl) => {
+    return acc + abs(vl - n);
+  }, 0);
+};
+
+const min_fuel = (arr) => {
+  let maxx = max(arr);
+
+  let min = -1;
+
+  for (let i = 0; i < maxx + 1; i++) {
+    let res = test_pos(i, arr);
+    if (res < min || min === -1) {
+      min = res;
+    }
+  }
+  return min;
+};
+
+console.log(min_fuel(array));
