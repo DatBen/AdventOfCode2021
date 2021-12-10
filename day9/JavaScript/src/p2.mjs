e9e9d373e286d25e6601bcd2871a55106e37f1f9
Serp <manu.serpette74@gmail.com>
2021-12-10 12:30:53 +0100

    p2 d9 ok

--- day9/JavaScript/src/p2.mjs
+++ day9/JavaScript/src/p2.mjs
@@ -1,60 +1,100 @@
 console.timeEnd("exec time");
 console.time("exec time");
 import { readFileSync } from "fs";
-var array = readFileSync("data/i").toString().split("\n");
+import { get } from "https";
+var array = readFileSync("data/input").toString().split("\n");
 const toInt = (arr) => arr.map((i) => parseInt(i, 10));
 array = array.map((vl) => {
   return toInt(vl.split(""));
 });
+const getPt = (arr, pt) => {
+  return arr[pt[1]][pt[0]];
+};
 
-const isTrou = (arr, x, y) => {
+const incl = (l, pt) => {
+  if (l.length == 0) {
+    return false;
+  }
+  return l.reduce((acc, vl) => {
+    if (vl[0] === pt[0] && vl[1] === pt[1]) {
+      return true;
+    } else {
+      return acc;
+    }
+  }, false);
+};
+
+const getNei = (arr, pt) => {
+  let x = pt[0];
+  let y = pt[1];
   if (x == 0) {
     if (y == 0) {
-      return arr[y][x] < arr[y][x + 1] && arr[y][x] < arr[y + 1][x];
+      return [
+        [x, y + 1],
+        [x + 1, y],
+      ];
     }
     if (y == arr.length - 1) {
-      return arr[y][x] < arr[y - 1][x] && arr[y][x] < arr[y][x + 1];
+      return [
+        [x, y - 1],
+        [x + 1, y],
+      ];
     }
-    return (
-      arr[y][x] < arr[y - 1][x] &&
-      arr[y][x] < arr[y][x + 1] &&
-      arr[y][x] < arr[y + 1][x]
-    );
+    return [
+      [x, y - 1],
+      [x, y + 1],
+      [x + 1, y],
+    ];
   }
   if (x == arr[0].length - 1) {
     if (y == 0) {
-      return arr[y][x] < arr[y][x - 1] && arr[y][x] < arr[y + 1][x];
+      return [
+        [x, y + 1],
+        [x - 1, y],
+      ];
     }
     if (y == arr.length - 1) {
-      return arr[y][x] < arr[y - 1][x] && arr[y][x] < arr[y][x - 1];
+      return [
+        [x - 1, y],
+        [x, y - 1],
+      ];
     }
-    return (
-      arr[y][x] < arr[y - 1][x] &&
-      arr[y][x] < arr[y][x - 1] &&
-      arr[y][x] < arr[y + 1][x]
-    );
+    return [
+      [x - 1, y],
+      [x, y - 1],
+      [x, y + 1],
+    ];
   }
   if (y == 0) {
-    return (
-      arr[y][x] < arr[y + 1][x] &&
-      arr[y][x] < arr[y][x + 1] &&
-      arr[y][x] < arr[y][x - 1]
-    );
+    return [
+      [x, y + 1],
+      [x - 1, y],
+      [x + 1, y],
+    ];
   }
   if (y == arr.length - 1) {
-    return (
-      arr[y][x] < arr[y - 1][x] &&
-      arr[y][x] < arr[y][x + 1] &&
-      arr[y][x] < arr[y][x - 1]
-    );
+    return [
+      [x - 1, y],
+      [x + 1, y],
+      [x, y - 1],
+    ];
   }
+  return [
+    [x, y - 1],
+    [x, y + 1],
+    [x - 1, y],
+    [x + 1, y],
+  ];
+};
 
-  return (
-    arr[y][x] < arr[y - 1][x] &&
-    arr[y][x] < arr[y][x + 1] &&
-    arr[y][x] < arr[y][x - 1] &&
-    arr[y][x] < arr[y + 1][x]
-  );
+const isTrou = (arr, pt) => {
+  return getNei(arr, pt).reduce((acc, vl) => {
+    if (getPt(arr, pt) >= getPt(arr, vl)) {
+      return false;
+    } else {
+      return acc;
+    }
+  }, true);
 };
 
 let ij = Array.from({ length: array[0].length }, (_, i) => i);
@@ -68,53 +108,49 @@ ij = ij
   .flat();
 
 let min = ij.reduce((acc, vl) => {
-  if (isTrou(array, vl[0], vl[1])) {
+  if (isTrou(array, vl)) {
     return [...acc, vl];
   } else {
     return acc;
   }
 }, []);
 
-const getPt = (arr, pt) => {
-  return arr[pt[1]][pt[0]];
-};
-
-const getNei = (arr, pt) => {
-  let x = pt[0];
-  let y = pt[1];
-  if (x == 0) {
-    if (y == 0) {
-      return [[y,x+1], [y + 1,x]];
-    }
-    if (y == arr.length - 1) {
-      return [[y - 1,x], [y,x + 1]];
-    }
-    return [[y - 1,x], [y,x + 1], [y + 1,x]];
-  }
-  if (x == arr[0].length - 1) {
-    if (y == 0) {
-      return [[y,x - 1], [y + 1,x]];
-    }
-    if (y == arr.length - 1) {
-      return [[y - 1,x], [y,x - 1]];
-    }
-    return [[y - 1,x], [y,x - 1], [y + 1,x]];
-  }
-  if (y == 0) {
-    return [y + 1,x], [y,x + 1], [y,x - 1];
-  }
-  if (y == arr.length - 1) {
-    return [[y - 1,x], [y,x + 1], [y,x - 1]];
-  }
-  return [[y - 1,x], [y,x + 1], [y,x - 1],[y+1,x]]
-};
-
 const basin = (arr, pt, set) => {
-  if (getPt(arr, pt) == 9) {
+  if (getPt(arr, pt) == 9 || incl(set, pt)) {
     return set;
+  } else {
+    set.push(pt);
+    getNei(arr, pt).forEach((vl) => {
+      set = basin(arr, vl, set);
+    });
   }
-};
 
-console.log(getNei(array,[50,50]));
+  return set;
+};
+console.log(min.length);
+const ans = (array, min) => {
+  min = min.map((vl) => {
+    return basin(array, vl, []).length;
+  });
+  let max = [0, 0, 0];
+  let id = 0;
+  max.forEach((_, i) => {
+    min.forEach((l, j) => {
+      if (l > max[i]) {
+        max[i] = l;
+        id = j;
+      }
+    });
+
+    min.splice(id, 1);
+  });
+  console.log(max);
+  console.log(
+    max.reduce((acc, vl) => {
+      return acc * vl;
+    }, 1)
+  );
+};
+ans(array, min);
 
 console.timeEnd("exec time");
